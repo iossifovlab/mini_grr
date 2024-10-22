43ca3b5dc38592318ca129dbf4bcc31f23a5e478
Ivan Iossifov <iossifov@cshl.edu>
2024-10-22 14:14:01 -0400

    Moved forward on the draw_position_scores.

--- draw_position_scores.py
+++ draw_position_scores.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
 import os
+import pylab
 from dae.genomic_resources import build_genomic_resource_repository
 from dae.genomic_resources.reference_genome import build_reference_genome_from_resource
 from dae.genomic_resources.genomic_scores import PositionScore
+
 
 
 # GRR = build_genomic_resource_repository()
@@ -28,10 +31,30 @@ for gr in GRR.get_all_resources():
 	print(gr.get_id())
 	sc = PositionScore(gr)
 	sc.open()
-
+	pylab.figure()
 	print(f"##### {gr.get_id()} #####")
-	for ch in ref.chromosomes:
-		for p in range(1, ref.get_chrom_length(ch)+1):
+	
+	for chi, ch in enumerate(ref.chromosomes):
+		pylab.subplot(len(ref.chromosomes), 1, chi+1)
+		xs = defaultdict(list)
+		ys = defaultdict(list)
+		chrom_len = ref.get_chrom_length(ch)
+		all_score_names = sc.get_all_scores()
+		for p in range(1, chrom_len+1):
 			print(ch, p, sc.fetch_scores(ch, p))
+			scores = sc.fetch_scores(ch, p)
+			if not (scores is None):
+				assert len(scores) == len(all_score_names)
+				for score_name, score in zip(all_score_names, scores):
+					if score:
+						xs[score_name].append(p)
+						ys[score_name].append(score)
+		for score_name in all_score_names:
+			pylab.scatter(xs[score_name],ys[score_name], label=score_name)
+		pylab.xlim([1-0.5, chrom_len+0.5])
+		pylab.xticks(range(1,chrom_len+1))
+		pylab.legend()
+		print(xs, ys)
+	pylab.savefig(f"{gr.get_id()}/scores_graph.png")
 
 
