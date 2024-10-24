9bf72aede0ec4e4111ab66b985bd330fc6b5c245
Ivan Iossifov <iossifov@cshl.edu>
2024-10-24 15:35:48 -0400

    Fixed bedgraph_tabix_0.

--- mini_positionscore_bedgraph_tabix_0/dataprep.sh
+++ mini_positionscore_bedgraph_tabix_0/dataprep.sh
@@ -1,4 +1,3 @@
 filename="mini_positionscore_bedgraph_0.bedgraph"
 bgzip -c "$filename" > ${filename}.gz
-# tabix -p bed -S 0 ${filename}.gz
-tabix -b 2 -e 3 -s 1 -0 -S 0 ${filename}.gz
+tabix -p bed ${filename}.gz
